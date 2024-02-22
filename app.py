55259cc7ff7e7139a28afbf2e63a4d2c6720bf1d
Cooper Spinelli <cooperspinelli@gmail.com>
2024-02-22 12:52:06 -0700

    Implement request handler for survey start page

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get("/")
+def survey_start():
+    """Returns survery start page"""
+    return render_template("survey_start.html",
+                           title=survey.title,
+                           instructions=survey.instructions)
+
+
+@app.post("/begin")
+def handle_begin():
+
+    return redirect("/questions/0")
+
